cc9fb91dd747ddbf44019a897bd43a10e1670df7
Hoàng Tuấn Minh <wheycoffee1607@gmail.com>
2024-01-01 16:34:08 +0700

    Databiding section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 // Component bản chất là một function return về một JSX mô tả giao diện sẽ được hiển thị
 
 // import Welcome from "./1_Component/Welcome";
+import Databiding from "./2_DataBinding/Databiding";
 import Home from "./BT_HomeLayout/Home";
 import Shop from "./BT_ShopLayout/Shop";
 
@@ -8,7 +9,7 @@ import Shop from "./BT_ShopLayout/Shop";
 function App() {
   return (
   <div>
-    <Shop/>
+    <Databiding/>
   </div>
   )
 }
